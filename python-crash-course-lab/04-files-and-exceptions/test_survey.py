7abe5939a947fe1049247c7aeb770532b6357a6f
Akbarov Damir <157966043+akbarovskii@users.noreply.github.com>
2026-09-06 14:43:39 +0500

    Update test_survey.py

--- python-crash-course-lab/04-files-and-exceptions/test_survey.py
+++ python-crash-course-lab/04-files-and-exceptions/test_survey.py
@@ -1,21 +1,22 @@
 import unittest
 from survey import AnonymousSurvey
-class TestAnonmyousSurvey(unittest.TestCase):
-    """Тест для класса AnonymousSurvey"""
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """Tests for the class AnonymousSurvey."""
 
     def setUp(self):
-        """Создание опроса и набора ответов для всех тестовых методов."""
+        """Create a survey and a set of responses for all test methods."""
         question = "What language did you first learn to speak?"
         self.my_survey = AnonymousSurvey(question)
         self.responses = ['English', 'Spanish', 'Mandarin']
 
     def test_store_single_response(self):
-        """Проверяет, что один ответ сохранен правильно."""
+        """Test that a single response is stored properly."""
         self.my_survey.store_response(self.responses[0])
         self.assertIn(self.responses[0], self.my_survey.responses)
 
-    def test_store_three_response(self):
-        """Проверяет, что три ответа сохранены правильно."""
+    def test_store_three_responses(self):
+        """Test that three responses are stored properly."""
         for response in self.responses:
             self.my_survey.store_response(response)
         for response in self.responses:
