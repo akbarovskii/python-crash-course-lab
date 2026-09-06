8c4c01d7c57748010a6ac52625016e866d6d3107
Akbarov Damir <157966043+akbarovskii@users.noreply.github.com>
2026-09-06 13:49:13 +0500

    Update alien_position.py

--- python-crash-course-lab/02-logic-and-loops/alien_position.py
+++ python-crash-course-lab/02-logic-and-loops/alien_position.py
@@ -1,17 +1,17 @@
 alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
 print(f"Original position: {alien_0['x_position']}")
 
-#Пришелец перемещается вправо.
-#Вычисляем величину смещения на основании текущей скорости.
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
 if alien_0['speed'] == 'slow':
     x_increment = 1
 elif alien_0['speed'] == 'medium':
     x_increment = 2
 else:
-    #Пришелец двигается быстро
+    # This is a fast alien.
     x_increment = 3
 
-#Новая позиция равна сумме старой позиции и приращения.
+# The new position is the old position plus the increment.
 alien_0['x_position'] = alien_0['x_position'] + x_increment
 
 print(f"New position: {alien_0['x_position']}")
